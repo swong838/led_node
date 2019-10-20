21ca4a95315668fd96fb3a0a383b63c3b0982a3b
swong <swong@digitalchemy.net>
2019-10-20 11:52:27 -0700

    move index file up, track it

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 const port = process.env.PORT || 5000;
 const server = express();
 
-server.use('/', express.static('./server/client/build'))
+server.use('/', express.static('./server/client'))
 
 server.listen(port, () => {
     console.log(`Listening on port ${port}`);
