09e979790d3b7af889056dce0a89238660b6efa0
swong <swong@digitalchemy.net>
2020-03-07 10:00:19 -0800

    testing console output

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import express from 'express';
 //import Pixel from 'client/src/lib/pixel';
 
 
-
 const port = process.env.PORT || 5000;
 const server = express();
 
@@ -12,30 +11,33 @@ const NUM_PIXELS = 10;
 
 
 server.use('/', express.static('./server/client'))
-
 server.listen(port, () => {
+    process.stdout.write(`Listening on port ${port}`);
+});
+
+
+(() => {
+    process.stdout.write("\n");
+    let count = 0;
     // let pixelArray = [];
     // for (let p = 0; p < NUM_PIXELS; p++){
     //     pixelArray.push(new Pixel(p));
     // }
 
-    process.stdout.write(`Listening on port ${port}`);
-    process.stdout.write("\n");
-    let count = NUM_PIXELS;
-    while (count++ % NUM_PIXELS) {
-        setTimeout(count => {
-            draw(count);
-        }, TICKRATE)
+    const draw = (count) => {
+        const glyph = '*';
+        let message = [];
+        for(let i = 0; i < count; i++) {
+           message.push(glyph);
+        }
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(message.join(''));
     }
-});
+    
+    setInterval(() => {
+        draw(count++);
+        count = count % NUM_PIXELS;
+    }, TICKRATE);
 
-
-const draw = count => {
-    const glyph = '*';
-    let message = [];
-    for(let i = 0; i < count; i++) {
-        message.append(glyph);
-    }
-    process.stdout.clearLine();
-    process.stdout.write(message.join(''));
-}
+})();
