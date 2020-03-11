d4020c44d3ca693333b25dd5f94f5af395591640
swong <swong@digitalchemy.net>
2020-03-10 20:18:07 -0700

    trying to rig up the wave effect for testing

--- application/server.js
+++ application/server.js
@@ -4,9 +4,10 @@ import diagnostics from '../src/lib/diagnostics';
 const port = process.env.PORT || 5000;
 const server = express();
 server.use('/', express.static('./application/client'))
+server.use('/wave', express.static('./application/client/wave.html'))
 server.listen(port, () => {
     process.stdout.write(`Listening on port ${port}`);
 });
 
 // display test output in stdout
-diagnostics(1);
+//diagnostics(1);
