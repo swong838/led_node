bfa3186a5e0d953bf82581a0c4a73263817cf3b8
swong <swong@digitalchemy.net>
2020-11-27 12:17:26 -0800

    converting wave to pointlight system

--- src/effects/wave/renderer_wave.js
+++ src/effects/wave/renderer_wave.js
@@ -5,115 +5,101 @@ import { randInt, log } from '../../lib/utilities';
 
 // effects
 import Wave from './wave';
+import PointLight from '../_common/point_light';
 
-
-const mask = (position, distance) => {
-    /**
-     * max(position) - get range +- distance from position
-     *    floor of 0, ceiling of ledStripLength
-     * 
-     * return
-     *    (array) [bottom, top]
-     */
-    const bottom = Math.floor(Math.max(position - distance, 0));
-    const top = Math.ceil(Math.min(position + distance, ledStripLength));
-    return [bottom, top];
-}
 
 const led_waves = (effectBuffer) => {
 
     const renderer = new Renderer();
 
-
-    const randomWave = () => {
-        return new Wave({
-            origin: randInt(STRIP_LENGTH),
+    const wave = () => {
+        // spawn two point lights going in opposite directions from the same origin
+        const right = {
+            position: randInt(STRIP_LENGTH),
             r: randInt(105)+ 10,
             g: randInt(105)+ 10,
             b: randInt(105)+ 10,
             velocity: (Math.random() * .02),
-            velocityFalloff: Math.min(Math.random(), .00002),
-            powerFalloff: (Math.random() * .125),
-        });
-    }
-
-    const mask = (position, distance) => {
-        /**
-         * max(position) - get range +- distance from position
-         *    floor of 0, ceiling of ledStripLength
-         * 
-         * return
-         *    (array) [bottom, top]
-         */
-        const bottom = Math.floor(Math.max(position - distance, 0));
-        const top = Math.ceil(Math.min(position + distance, ledStripLength));
-        return [bottom, top];
-    }
-
-
-    let waves = [];
-
-
-    const advance = () => {
-        waves = waves.filter(wave => {
-            if (!wave.alive) {
-                const morgueTime = new Date().getTime() - wave.expirationTime;
-                log(`${wave.origin} died: ${wave.obit}, cleanup took ${morgueTime}ms`);
-            }
-            return wave.alive;
-        });
-
-        // propagate each effect that remains
-        waves.forEach(wave => wave.propagate());
-    };
-
-    const render = () => {
-
-        let touched = {};
-        waves.forEach(wave => {
-            const [leftBottom, leftTop] = mask(wave.leftEdge, wave.distanceCutoff);
-            const [rightBottom, rightTop] = mask(wave.rightEdge, wave.distanceCutoff);
-
-            for (let i = leftBottom; i <= leftTop; i++) {
-                const {r, g, b} = wave.poll(i);
-                if (!touched[i]) {
-                    touched[i] = {r: 0, g: 0, b: 0}
-                }
-                touched[i].r += r;
-                touched[i].g += g;
-                touched[i].b += b;
-            }
-
-            for (let i = rightBottom; i <= rightTop; i++) {
-                const {r, g, b} = wave.poll(i);
-                if (!touched[i]) {
-                    touched[i] = {r: 0, g: 0, b: 0}
-                }
-                touched[i].r += r;
-                touched[i].g += g;
-                touched[i].b += b;
-            }
-        });
-
-        ledStrip.zero();
-        for (const index in touched) {
-            const {r, g, b} = touched[index];
-            ledStrip.setLED(index, r, g, b);
+            velocity_delta: function(){ this.velocity *= Math.min(Math.random(), .00002); },
+            r_delta: function(){ this.r -= Math.random() * .125; },
+            g_delta: function(){ this.g -= Math.random() * .125; },
+            b_delta: function(){ this.b -= Math.random() * .125; },
+        };
+        const left = {
+            ...right,
+            velocity: -right.velocity,
         }
-
-        ledStrip.sync();
+        return [new PointLight(left), new PointLight(right)];
     }
 
-    //effect generator
+    // const mask = (position, distance) => {
+    //     /**
+    //      * max(position) - get range +- distance from position
+    //      *    floor of 0, ceiling of ledStripLength
+    //      * 
+    //      * return
+    //      *    (array) [bottom, top]
+    //      */
+    //     const bottom = Math.floor(Math.max(position - distance, 0));
+    //     const top = Math.ceil(Math.min(position + distance, ledStripLength));
+    //     return [bottom, top];
+    // }
+
+    // const advance = () => {
+    //     waves = waves.filter(wave => {
+    //         if (!wave.alive) {
+    //             const morgueTime = new Date().getTime() - wave.expirationTime;
+    //             log(`${wave.origin} died: ${wave.obit}, cleanup took ${morgueTime}ms`);
+    //         }
+    //         return wave.alive;
+    //     });
+
+    //     // propagate each effect that remains
+    //     waves.forEach(wave => wave.propagate());
+    // };
+
+    // const render = () => {
+
+    //     let touched = {};
+    //     waves.forEach(wave => {
+    //         const [leftBottom, leftTop] = mask(wave.leftEdge, wave.distanceCutoff);
+    //         const [rightBottom, rightTop] = mask(wave.rightEdge, wave.distanceCutoff);
+
+    //         for (let i = leftBottom; i <= leftTop; i++) {
+    //             const {r, g, b} = wave.poll(i);
+    //             if (!touched[i]) {
+    //                 touched[i] = {r: 0, g: 0, b: 0}
+    //             }
+    //             touched[i].r += r;
+    //             touched[i].g += g;
+    //             touched[i].b += b;
+    //         }
+
+    //         for (let i = rightBottom; i <= rightTop; i++) {
+    //             const {r, g, b} = wave.poll(i);
+    //             if (!touched[i]) {
+    //                 touched[i] = {r: 0, g: 0, b: 0}
+    //             }
+    //             touched[i].r += r;
+    //             touched[i].g += g;
+    //             touched[i].b += b;
+    //         }
+    //     });
+
+    //     ledStrip.zero();
+    //     for (const index in touched) {
+    //         const {r, g, b} = touched[index];
+    //         ledStrip.setLED(index, r, g, b);
+    //     }
+
+    //     ledStrip.sync();
+    // }
+
+    // main render loop
     setInterval(() => {
-        advance();
-        if (waves.length) {
-            render();
-        }
-
+        renderer.tick();
         if (waves.length < 8 && Math.random() * 1002 > 900) {
-            const newWave = randomWave();
-            waves.push(newWave);
+            renderer.effects.push(...wave());
         }
     }, TICKRATE);
 
